48f08863027fc60e536a93642b419d1aa43eef66
nkamble <nkamble@iu.edu>
2021-11-19 04:08:30 -0500

    assignment 5 done 1. comments and testbed remaining

---
+++ src/gobackn.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "file.h"
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <ctype.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+void gbn_server(char* iface, long port, FILE* fp) {
+    
+}
+
+void gbn_client(char* host, long port, FILE* fp) {
+
+}
